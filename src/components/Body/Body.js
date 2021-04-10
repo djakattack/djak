97ad521f49006989d2ca51bdcad124d99d50aab3
jpearce2014 <jlpearce.phd@gmail.com>
2021-04-10 13:42:01 -0400

    Having issues with flex-direction in SASS.  Results do not match tutorial video.

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Body = ({ props }) => (
     <div className="light">
-        <div className="content__paragraph">
+        <div className="content__paragraph1">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam corrupti optio dolorem porro mollitia alias maiores laboriosam debitis natus inventore harum, architecto delectus quidem pariatur saepe quas perspiciatis praesentium est.
         </div>
-        <div className="content__paragraph">
+        <div className="content__paragraph2">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam corrupti optio dolorem porro mollitia alias maiores laboriosam debitis natus inventore harum, architecto delectus quidem pariatur saepe quas perspiciatis praesentium est.
         </div>
     </div>
